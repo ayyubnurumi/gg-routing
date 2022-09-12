400d09f54e39aef15905100d3e58fd0f2f78f5b8
ayyubnurumi <ayyubinvestment@gmail.com>
2022-09-12 18:40:01 +0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import { Dashboard } from './Pages/Dashboard';
+import { Login } from './Auth/Login';
+import { Registration } from './Auth/Registration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/registration' element={<Registration />} />
+        </Route>
+        <Route>
+          <Route path='/' element={<Dashboard />} />
+        </Route>
+        <Route path='*' element={<Navigate to={'/'} replace />} />
+      </Routes>
+    </Router>
   );
 }
 
